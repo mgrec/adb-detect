76e5966516b1dbbe796b4886e409187088be8554
Grec Maxime <maxime.grec@gmail.com>
2016-12-30 10:59:26 +0100

    add comment

--- adb-detect-1.0.js
+++ adb-detect-1.0.js
@@ -2,11 +2,17 @@
  * Created by maxime on 30/12/2016.
  */
 $(document).ready(function () {
+
+    // Insert iframe with fake url
     $('body').append('<iframe src="http://billy-cocaine.dkz/' + Math.floor((Math.random() * 666) + 666) + '/ads.php"  frameborder="0" scrolling="no" id="myFrame"></iframe>');
+
+    // Hide iframe if adblock is disable
     $('#myFrame').css('width', '0px');
     setTimeout(function () {
+
+        // Test if adblock hide the iframe
         if ($('#myFrame') == null || $('#myFrame').css('visibility') == 'hidden' || $('#myFrame').css('display') == 'hidden' || $('#myFrame').css('display') == 'none') {
-            $('body').text('desactiver votre adblocker');
+            $('body').text('desactiver votre adblock');
         } else {
             
         }
